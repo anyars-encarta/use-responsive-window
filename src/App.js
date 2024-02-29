0983df4e80e3740c326377c6cdae2aebf70719ad
anyars-encarta <anyarsencarta@gmail.com>
2024-02-29 16:47:10 +0000

    Correct eslint errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import UseResponsiveWindowTest from './components/responsive-window/test';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <UseResponsiveWindowTest />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <UseResponsiveWindowTest />
+  </div>
+);
 
 export default App;
